99640e335e39cb04626d66912f4510f39d6a26d5
chuduc thien <thiencnt49@gmail.com>
2019-01-24 09:12:25 +0700

    them kho hang startup

---
+++ Absoft/Repositories/Interfaces/INhapVatTuRepository.cs
@@ -0,0 +1,17 @@
+﻿using Absoft.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Absoft.Repositories.Interfaces
+{
+    public interface INhapVatTuRepository
+    {
+        Task<List<NhapVatTuViewModel>> GetAllAsync();
+        Task<NhapVatTuViewModel> GetById(int id);
+        Task<bool> InsertAsync(NhapVatTuViewModel mdonvitinh);
+        Task<bool> UpdateAsync(NhapVatTuViewModel mdonvitinh);
+        Task<bool> DeleteAsync(int id);
+    }
+}
