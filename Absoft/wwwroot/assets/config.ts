f5380aeebec766667005213cba579813ef892762
AnhBH <jangkyavn@gmail.com>
2019-02-21 14:23:57 +0700

    add ingonre

--- Absoft/wwwroot/assets/config.ts
+++
@@ -1,9 +0,0 @@
-export class Config {
-    public static getDomain() {
-        return 'localhost:44379'; // localhost:44379 qlvtbv2.theappnow.net
-    }
-
-    public static getHttp() {
-        return 'https';
-    }
-}
