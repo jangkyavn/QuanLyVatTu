5bc56feb33753e88685aa97fecd167eaa4466f99
AnhBH <jangkyavn@gmail.com>
2019-02-21 14:28:27 +0700

    update ignore

---
+++ Absoft/wwwroot/assets/config.ts
@@ -0,0 +1,9 @@
+export class Config {
+    public static getDomain() {
+        return 'localhost:44379'; // localhost:44379 qlvtbv2.theappnow.net
+    }
+
+    public static getHttp() {
+        return 'https';
+    }
+}
