0a0afb9d115af1a8bab00fc499298dd0ccc1684e
AnhBH <jangkyavn@gmail.com>
2019-02-21 10:11:38 +0700

    change type chietkhau

---
+++ Absoft.Data/Migrations/20190221030945_ChangeTypeChietKhau.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Absoft.Data.Migrations
+{
+    public partial class ChangeTypeChietKhau : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "ChietKhau",
+                table: "XuatVatTu",
+                nullable: true,
+                oldClrType: typeof(float),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "ChietKhau",
+                table: "NhapVatTu",
+                nullable: true,
+                oldClrType: typeof(float),
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<float>(
+                name: "ChietKhau",
+                table: "XuatVatTu",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<float>(
+                name: "ChietKhau",
+                table: "NhapVatTu",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldNullable: true);
+        }
+    }
+}
